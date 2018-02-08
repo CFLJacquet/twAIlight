ca5ad731f2c8d01895a0774e55a27c5a18335857
Seris <mathieu.seris@student.ecp.fr>
2018-02-08 15:59:21 +0100

    minmax v0

--- minimax.py
+++ minimax.py
@@ -11,14 +11,14 @@ class Morpion:
     Par défaut, True commence"""
 
     def __init__(self):
-        self.etats = list() # contenu du plateau, dans l'ordre dans lequel les pions sont joués
+        self.previous_moves = list()  # contenu du plateau, dans l'ordre dans lequel les pions sont joués
 
     def whos_turn(self):
         """ Renvoie le joueur qui a la main.
 
         :return: boolean: nom du joueur
         """
-        if len(self.etats) % 2:
+        if len(self.previous_moves) % 2:
             return False
         else:
             return True
@@ -44,62 +44,65 @@ class Morpion:
             i, j = move
             player = self.whos_turn()
 
-        self.etats.append((i, j, player))
+        self.previous_moves.append((i, j, player))
 
-    def __copy__(self, objet):
-        t = deepcopy(objet)
-        return t
-
-    def state_evaluation(self, curr_player):
+    def state_evaluation(self):
         """ Renvoie l'évaluation d'une carte Morpion pour le joueur actuel
 
         :param curr_player: race du joueur actuel (boolean)
         :return: score de l'évaluation
         """
-        other_player = not curr_player
 
         # Cas victoire
-        if self.winner() == curr_player:
-            return 10000
+        if self.winner():
+            return 10
 
         # Cas défaite
-        elif self.winner() == other_player:
-            return -10000
+        elif self.winner() == False:
+            return -10
 
         # Cas Partie en cours
         else:
-            op_c = self.open_positions(curr_player)
-            op_o = self.open_positions(other_player)
+            op_c = self.open_positions(True)
+            op_o = self.open_positions(False)
         return op_o - op_c
 
-    def open_positions(self,player):
-        count=0
-        row_line_blanks=[]
+    def open_positions(self, player):
+        count = 0
         other_player = not player
 
         for i in range(3):
             # Quoiqu'il arrive on ajoute toutes les lignes blanches ou colonnes blanche
-            if i not in [k[0] for k in self.etats]:
-                count=count+1
-            if i not in [k[1] for k in self.etats]:
-                count=count+1
-            # On parcourt ensuite les état qui pourraient donner lieu à une victoire
+            if i not in [k[0] for k in self.previous_moves]:
+                count = count + 1
+            if i not in [k[1] for k in self.previous_moves]:
+                count = count + 1
+                # On parcourt ensuite les état qui pourraient donner lieu à une victoire
 
-        #print("Je ne garde que mes états")
-        #print([state for state in self.etat if state[2]!=other_player])
-        for i in [state for state in self.etats if state[2]!=other_player]:
+        # print("Je ne garde que mes états")
+        # print([state for state in self.etat if state[2]!=other_player])
+        for i in [state for state in self.previous_moves if state[2] != other_player]:
             # si une ligne n'est occupée que par nos pions, c'est une victoire potentielle
-            #print([k[0] for k in self.etat if k!=i and k[2]==other_player])
-            if i[0] not in [k[0] for k in self.etats if k!=i and k[2]==other_player]:
-                count=count+1
+            # print([k[0] for k in self.etat if k!=i and k[2]==other_player])
+            if i[0] not in [k[0] for k in self.previous_moves if k != i and k[2] == other_player]:
+                count = count + 1
             # Si une colonne n'est occupée que par nos pions c'est aussi une victoire potentielle
-            if i[1] not in [k[1] for k in self.etats if k!=i and k[2]==other_player]:
-                count=count+1
+            if i[1] not in [k[1] for k in self.previous_moves if k != i and k[2] == other_player]:
+                count = count + 1
+
         # Cas particulier des diagonales
-        if (1,1,other_player) in self.etats:
-            # Si l'autre n'occupe pas le centre (donc soit nous l'occupons, soit il est vide), on regarde qui occupe les coins
-            if other_player not in [k[2] for k in self.etats if k[0:2]==(0,1) or k[0:2]==(0,2) or k[0:2]==(2,0) or k[0:2]==(2,2)]:
-                count=count+1
+        # On vérifie la non présence d'une case détenue par l'ennemi sur chacune des diagonales
+
+        # Diagonale descendante
+        if not ((1, 1, other_player) in self.previous_moves or (0, 0, other_player) in self.previous_moves or (
+                2, 2, other_player) in self.previous_moves):
+            count += 1
+
+        # Diagonale montante
+        if not ((1, 1, other_player) in self.previous_moves or (2, 0, other_player) in self.previous_moves or (
+                0, 2, other_player) in self.previous_moves):
+            count += 1
+
         return count
 
     def winner(self):
@@ -112,98 +115,134 @@ class Morpion:
         for i in range(3):
 
             # Pour la ligne i, on cherche le joueur qui l'occupe
-            if (i, 0, True) in self.etats:
+            if (i, 0, True) in self.previous_moves:
                 current_player = True
-            elif (i, 0, False) in self.etats:
+            elif (i, 0, False) in self.previous_moves:
                 current_player = False
             else:
                 break
 
             # On regarde les deux autres éléments de la colonne pour vérifier si elle est occupée par le même joueur
-            if (i, 1, current_player) in self.etats and (i, 2, current_player) in self.etats:
+            if (i, 1, current_player) in self.previous_moves and (i, 2, current_player) in self.previous_moves:
                 return current_player
 
         # Parcours de la première colonne pour observer les lignes
         for j in range(3):
 
             # Pour la colonne j, on cherche le joueur qui l'occupe
-            if (0, j, True) in self.etats:
+            if (0, j, True) in self.previous_moves:
                 current_player = True
-            elif (0, j, False) in self.etats:
+            elif (0, j, False) in self.previous_moves:
                 current_player = False
             else:
                 break
 
             # On regarde les deux autres éléments de la ligne pour vérifier si elle est occupée par le même joueur
-            if (1, j, current_player) in self.etats and (2, j, current_player) in self.etats:
+            if (1, j, current_player) in self.previous_moves and (2, j, current_player) in self.previous_moves:
                 return current_player
 
         # Cas des diagonales
 
         # Joueur pouvant avoir une diagonale
-        if (1, 1, True) in self.etats:
+        if (1, 1, True) in self.previous_moves:
             current_player = True
-        elif (1, 1, False) in self.etats:
+        elif (1, 1, False) in self.previous_moves:
             current_player = False
         else:
             return None
 
         # Parcours des deux diagonales avec l'information du joueur qui a le centre
-        if (0, 0, current_player) in self.etats and (2, 2, current_player) in self.etats:
+        if (0, 0, current_player) in self.previous_moves and (2, 2, current_player) in self.previous_moves:
             return current_player
-        if (2, 0, current_player) in self.etats and (0, 2, current_player) in self.etats:
+        if (2, 0, current_player) in self.previous_moves and (0, 2, current_player) in self.previous_moves:
             return current_player
 
         return None
 
     def next_possible_moves(self):
+        """ Renvoie l'ensemble des positions possibles sur la carte pour un prochain mouvement.
+        Pas besoin de préciser le joueur qui va jouer !
+
+        :return: set
+        """
+        # Toutes les cases de la carte...
         possible_moves = set((i, j) for i in range(3) for j in range(3))
-        possible_moves -= set((i, j) for i, j, _ in self.etats)
+
+        # Auxquelles on supprime les cases déjà occupées
+        possible_moves -= set((i, j) for i, j, _ in self.previous_moves)
+
+        # Race du joueur qui va jouer
         next_player = self.whos_turn()
+
         return set((i, j, next_player) for (i, j) in possible_moves)
 
     def game_over(self):
+        """ Renvoie Vrai si le jeu est terminé, Faux sinon
+
+        :return: boolean
+        """
+
+        # Parcours des colonnes
+
+        # Sélection du potentiel vainqueur avec le parcours de la première ligne
         for i in range(3):
-            if (i, 0, True) in self.etats:
+            if (i, 0, True) in self.previous_moves:
                 current_player = True
-            elif (i, 0, False) in self.etats:
+            elif (i, 0, False) in self.previous_moves:
                 current_player = False
             else:
                 break
-            if (i, 1, current_player) in self.etats and (i, 2, current_player) in self.etats:
+            # Parcours de la colonne
+            if (i, 1, current_player) in self.previous_moves and (i, 2, current_player) in self.previous_moves:
                 return True
 
+        # Idem avec les lignes en parcourant la première colonne
         for j in range(3):
-            if (0, j, True) in self.etats:
+            if (0, j, True) in self.previous_moves:
                 current_player = True
-            elif (0, j, False) in self.etats:
+            elif (0, j, False) in self.previous_moves:
                 current_player = False
             else:
                 break
-            if (1, j, current_player) in self.etats and (2, j, current_player) in self.etats:
+            if (1, j, current_player) in self.previous_moves and (2, j, current_player) in self.previous_moves:
                 return True
 
-        if (1, 1, True) in self.etats:
+        # Cas des diagonales
+
+        # Qui controle le centre ?
+        if (1, 1, True) in self.previous_moves:
             current_player = True
-        elif (1, 1, False) in self.etats:
+        elif (1, 1, False) in self.previous_moves:
             current_player = False
         else:
-            return False
-        if (0, 0, current_player) in self.etats and (2, 2, current_player) in self.etats:
+            return False  # Partie non terminée
+
+        # Le joueur occupant le centre, détient la diagonale descendante ?
+        if (0, 0, current_player) in self.previous_moves and (2, 2, current_player) in self.previous_moves:
             return True
-        if (2, 0, current_player) in self.etats and (0, 2, current_player) in self.etats:
+
+        # Le joueur occupant le centre, détient la diagonale montante ?
+        if (2, 0, current_player) in self.previous_moves and (0, 2, current_player) in self.previous_moves:
+            return True
+
+        # Si la grille est complète
+        if len(self.previous_moves) == 9:
             return True
 
         return False
 
     def __repr__(self):
+        """ Représente le plateau du Morpion
+
+        :return:
+        """
         res = "-------\n"
         for j in range(3):
             for i in range(3):
                 res += '|'
-                if (i, j, True) in self.etats:
+                if (i, j, True) in self.previous_moves:
                     res += "X"
-                elif (i, j, False) in self.etats:
+                elif (i, j, False) in self.previous_moves:
                     res += "O"
                 else:
                     res += " "
@@ -211,24 +250,13 @@ class Morpion:
         return res
 
 
-class GrapheDeJeu:
-    def __init__(self):
-        self.noeuds = []
-
-    def add_vertice(self, vertice):
-        if isinstance(vertice, SommetDuJeu):
-            self.noeuds.append(vertice)
-
-    def add_vertices(self, vertices):
-        for vert in vertices:
-            self.add_vertice(vert)
-
-
 class SommetDuJeu:
-    def __init__(self, is_ami):
-        self.children = list()
+    def __init__(self, is_ami=True):
+        self.parent = None
+        self._children = list()
         self.alpha = None
         self.beta = None
+        self._score = None
         self.etat = Morpion()
         self.is_ami = is_ami
 
@@ -238,96 +266,81 @@ class SommetDuJeu:
 
     # MaxValue et MinValue vont devoir utiliser un parcours de graph type DFS
     def MinValue(self):
-        pass
+        if self.etat.game_over():
+            return self.score
+        else:
+            children_scores = [child.MaxValue() for child in self.children]
+            return min(children_scores)
 
     def MaxValue(self):
-        val = self.etat.state_evaluation(self.is_ami)
-        return val
+        if self.etat.game_over():
+            return self.score
+        else:
+            children_scores = [child.MinValue() for child in self.children]
+            return max(children_scores)
 
+    @property
+    def score(self):
+        if self._score is None:
+            self._score = self.etat.state_evaluation()
+        return self._score
 
-def select_move_to_study(self, data):
-    return data
+    @property
+    def children(self):
+        # Si la liste des enfants n'est pas vide, alors nul besoin de la recalculer !
+        if self._children:
+            return self._children
+        # Si la liste est vide alors on la recalcule
+        else:
+            for move in self.etat.next_possible_moves():
+                next_ami = not self.is_ami
 
+                # Création du sommet fils
+                new_child_vertice = SommetDuJeu(next_ami)
 
-def get_graph(morpion_state, is_ami, level, total_level, child_vertice, graph_du_jeu, i):
-    """
-    Fonction de generation recursive de graph
-    A un level
-    """
-    # On genere un graph dont la racine est morpion_state
-    cur_vertice = SommetDuJeu(is_ami)
-    cur_vertice.etat = cur_vertice.etat.__copy__(morpion_state)
-    graph_du_jeu.add_vertice(cur_vertice)
-    is_ami = not is_ami
-    child_vertice[level] = {}
-    # on commence par le base case: l'etat que l'on nous envoie est il un etat final ?
-    if morpion_state.game_over() or level >= total_level:
-        # dans ce cas la on s'arrete et on sort de la boucle
-        pass;
+                # On met la partie du sommet fils à jour
+                moves = self.etat.previous_moves + [move]
+                new_child_vertice.etat.add_moves(moves)
 
-    else:
-        # Dans ce cas on peut rajouter des vertices avec des etats
-        # On ajoute chacun des états possibles qui peut succéder à l'état actuel
-        for move in cur_vertice.etat.next_possible_moves():
-            # On copie l'état actuel du jeu:
-            child_vertice[level][i] = SommetDuJeu(is_ami)
-            child_vertice[level][i] = child_vertice[level][i].__copy__(cur_vertice)
-            # On ajoute au nouvel état le nouveau move possible
-            child_vertice[level][i].etat.add_move(move)
+                # On ajoute ce fils complété dans la liste des fils du noeud actuel
+                self._children.append(new_child_vertice)
 
-            # On lie au noeud père le noeuf enfant
-            cur_vertice.children.append(child_vertice[level][i])
-            graph_du_jeu.add_vertice(child_vertice[level][i])
-            # On ajoute les noeufs enfants du noeuf enfant
+            return self._children
 
-            get_graph(child_vertice[level][i].etat, is_ami, level + 1, total_level, child_vertice, graph_du_jeu, i + 1)
+    def next_move(self):
+        """ Renvoie le meilleur mouvement à faire.
+        C'est la fonction Minimax-Decision du cours 4 s.54
 
-    return graph_du_jeu
+        Parcourt le graphe en DFS
 
+        :return: le prochain mouvement
+        """
 
-def minimax(morpion_state, is_ami):
-    """
-    cette fonction doit retourner le meilleur mouvement étant donné un état du jeu
-    """
-    # On commence par générer un graph avec tous les états du jeu
-    horizon = 8
-    graph_du_jeu = GrapheDeJeu()
-    graph = get_graph(morpion_state, is_ami, 0, horizon, {}, graph_du_jeu, 0)
-    print(graph)
-    # On parcourt ce graph et on regarde la Max,MinValue de chacun des etats feuilles
-    Q = Queue()
-    # Initialisation du parcours de graph
-    Q.put(graph.noeuds[0])
-    # parcours de graph
-    print("Parcours de graph")
-    max_val=0
-    while not Q.empty():
-        cur=Q.get()
-        val = cur.MaxValue()
+        # On sélectione le noeud fils selon sa race
+        if self.is_ami:
+            next_child = max(self.children, key=lambda x: x.MinValue())
+        else:
+            next_child = min(self.children, key=lambda x: x.MaxValue())
 
-        if val > max_val:
-            max_val=val
-            print("best configuration updated:")
-            print(cur.etat.etats)
-            best_scenario = cur.etat.etats
-        # Ajout des enfants
-        for c in cur.children:
-            Q.put(c)
-
-    # il faut faire remonter certaines valeurs et baisser les autres
-
-    return best_scenario
+        # On retourne le dernier mouvement pour arriver à ce sommet fils
+        return next_child.etat.previous_moves[-1]
 
 
 if __name__ == "__main__":
-    morpion = Morpion()
-    print(morpion.game_over())
-    print(morpion.next_possible_moves())
-    morpion.add_moves([(0, 1),(2,0)])
-    count=0
-    #print(morpion.state_evaluation(False))
-    best_next_move = minimax(morpion,False)
-    morpion.add_move(best_next_move[-1])
-    print(morpion)
-    #print(morpion.game_over())
-    #print(morpion.winner())
+    sommet = SommetDuJeu()
+
+    while not sommet.etat.game_over():
+        print(sommet.etat)
+        print("{} joue".format(sommet.is_ami))
+
+        previous_moves=list(sommet.etat.previous_moves)
+        previous_ami=sommet.is_ami
+
+        updated_moves=previous_moves+[sommet.next_move()]
+
+        sommet=SommetDuJeu(not previous_ami)
+        sommet.etat.add_moves(updated_moves)
+
+    print(sommet.etat)
+    print("Vainqueur : {}".format(sommet.etat.winner()))
+
