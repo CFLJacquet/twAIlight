dd97da2fda40f104d2d9a5b53482337eead43aa5
Seris <mathieu.seris@student.ecp.fr>
2018-03-05 15:17:41 +0100

    debug most probable moves

--- Algorithmes/Sommet_du_jeu_NegaMax_probable_outcome.py
+++ Algorithmes/Sommet_du_jeu_NegaMax_probable_outcome.py
@@ -36,8 +36,9 @@ class SommetDuJeu_NegaMax(SommetOutcome):
         if self._children is None:
             self._children = list()
             for moves in self.map.next_possible_moves(self.is_vamp):
-
-                child = SommetDuJeu_Negamax(is_vamp=not self.is_vamp, depth=self.depth-1, game_map=deepcopy(self.map.most_probable_outcome(moves)))
+                carte=deepcopy(self.map)
+                carte.most_probable_outcome(moves)
+                child = SommetDuJeu_NegaMax(is_vamp=not self.is_vamp, depth=self.depth-1, game_map=carte)
                 child.previous_moves = moves
                 self._children.append(child)
         return self._children
@@ -129,6 +130,6 @@ class SommetDuJeu_NegaMax(SommetOutcome):
 if __name__ == '__main__':
     carte = Map()
     racine= SommetDuJeu_NegaMax(depth=2, game_map=carte, is_vamp=True, init_map=True)
-    import cProfile
-    cProfile.run('racine.next_move()')
-    print(racine.next_move())
+    for child in racine.children:
+        print(child.previous_moves)
+        child.map.print_map()
