affa1dbfb05ea8176271626bbe214fa552d97944
Seris <mathieu.seris@student.ecp.fr>
2018-03-20 17:52:00 +0100

    création MCTS MPOO (non fonctionnel)

---
+++ Algorithmes/Algo_MCTS_MPOO.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from copy import deepcopy
+from time import time
+
+from Joueur_Interne import JoueurInterne
+from Serveur_Interne import ServeurInterne
+from Algorithmes.Sommet_du_jeu_MCTS_MPOO import SommetOutcome_MonteCarlo, SommetChance_MonteCarlo
+from Cartes.Map_Dust2 import MapDust2
+from Cartes.Map_TheTrap import MapTheTrap
+from Cartes.Map_Map8 import Map8
+from Cartes.Map_Random import MapRandom
+
+
+class AlgoAleatoireInterne(JoueurInterne):
+    """
+    Joueur avec la fonction de décision aléatoire de Charles
+    """
+
+
+class AlgoMCTSMPOO(JoueurInterne):
+    """
+    Une réécriture de la classe JoueurInterne
+
+    """
+    
+    def next_moves(self, show_map=True):
+        if show_map: self.map.print_map()
+
+        racine = SommetOutcome_MonteCarlo(game_map=deepcopy(self.map), is_vamp=self.is_vamp)
+
+        start_time = time()
+        while time() < start_time + 2:
+            racine.MCTS()
+
+        max_child = max(racine.children,
+                        key=lambda child: child.n_wins / child.n_games if child.n_games != 0 else 0)
+        robust_child = max(racine.children, key=lambda child: child.n_games)
+
+        if max_child == robust_child:
+            return max_child.previous_moves
+        else:
+            n_threshold = max_child.n_games / 4
+            selected_children = filter(lambda child: child.n_games > n_threshold, racine.children)
+            return max(selected_children,
+                       key=lambda child: child.n_wins / child.n_games if child.n_games != 0 else 0).previous_moves
+
+    @classmethod
+    def nb_vertices_created(cls):
+        return SommetOutcome_MonteCarlo.nb_vertices_created() + SommetChance_MonteCarlo.nb_vertices_created()
+
+
+if __name__ == "__main__":
+    Joueur1 = AlgoAleatoireInterne
+    Joueur2 = AlgoMCTSMPOO
+    Carte = MapRandom
+    Serveur = ServeurInterne(Carte, Joueur1, Joueur2, name1="Aléatoire", name2="MonteCarlo", print_map=True,
+                             debug_mode=False)
+    Serveur.start()
+    Serveur.join()
+    print(AlgoMCTSMPOO.nb_vertices_created())
